dbdf1c419c83e61c890a3f5f471a568c659b2e17
Nataliia Sokolova <newbeadwork@gmail.com>
2020-11-18 15:55:43 -0800

    Begininng analysis file

---
+++ EmployeeSQL/data_analysis.sql
@@ -0,0 +1,32 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT *
+FROM employees;
+
+SELECT employees.emp_no, employees.first_name, employees.last_name, employees.sex, salaries.salary
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no=salaries.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT  first_name, last_name, hire_date
+FROM    employees
+WHERE   hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
