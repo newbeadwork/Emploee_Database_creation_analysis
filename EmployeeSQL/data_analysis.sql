a2b55421223d2a992f6e471c969493d7b97b642e
Nataliia Sokolova <newbeadwork@gmail.com>
2020-11-18 18:25:30 -0800

    Updated Analysis file

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -1,10 +1,8 @@
 --List the following details of each employee: employee number, last name, first name, sex, and salary.
-SELECT *
-FROM employees;
 
 SELECT employees.emp_no, employees.first_name, employees.last_name, employees.sex, salaries.salary
-FROM salaries
-INNER JOIN employees ON
+FROM employees
+INNER JOIN salaries ON
 employees.emp_no=salaries.emp_no;
 
 --List first name, last name, and hire date for employees who were hired in 1986.
@@ -16,9 +14,20 @@ WHERE   hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
+SELECT  dm.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM    employees e
+INNER JOIN dept_manager dm ON 
+e.emp_no = dm.emp_no
+INNER JOIN departments d ON 
+dm.dept_no = d.dept_no;
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+SELECT  e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM    employees e
+INNER JOIN dept_employees de ON 
+e.emp_no = de.emp_no
+INNER JOIN departments d ON 
+de.dept_no = d.dept_no;
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
